3a184147588253116beb85c5caaca247a7e21060
Adigun Ibrahim <adigunibrahim574@gmail.com>
2021-02-10 10:45:36 +0100

    added PORT

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const UsersRoutes = require('./routes/users')
 const OrdersRoute = require('./routes/orders')
 const PaymentRoute = require('./routes/payments')
 
+const port = process.env.PORT || 3000
+
 var bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
@@ -16,4 +18,4 @@ app.use('/orders', OrdersRoute)
 
 app.use('/payments', PaymentRoute)
 
-app.listen(3000, ()=>console.log("server running on port 3000"))
+app.listen(port, ()=>console.log("server running on port 3000"))
